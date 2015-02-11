9314bd30e6a73754fa4e974cc64710c33f2d9196
Sam <sam@T60.(none)>
2015-02-11 17:55:10 +0100

    Fusion des fichiers et ajout sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<div class="sidebar">
+		Navbar : Prout ! Prout ! Prout ! Prout ! Prout !
+	<!-- <ul>
+
+		<li id="calendar"><h2>Calendrier</h2>
+			<?php get_calendar(); ?>
+		</li>
+		<li><h2>Categories</h2>
+			<ul>
+				<?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
+			</ul>
+			<?php wp_list_pages('title_li=<h2>Pages</h2>'); ?>
+		</li>
+	</ul> -->
+</div>
