c7e3fc2d84bbbcc9c542bad02358ffb3859aee9f
Enireves <enireves.carre@gmail.com>
2015-02-11 16:32:06 +0100

    Ajouté éléments de base aux fichiers

---
+++ footer.php
@@ -0,0 +1,10 @@
+			<footer class="footer">
+				<div class="widget_footer">
+			  <?php dynamic_sidebar('zone_widget_footer'); ?>
+				</div>
+				<?php wp_footer(); ?>
+			</footer>
+
+		</div>
+	</body>
+</html>
