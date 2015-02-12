c7074ea23235aa0fbddc2fd740bbc502909438ea
Enireves <enireves.carre@gmail.com>
2015-02-12 09:18:27 +0100

    Supprimé les fichiers sidebar_header.php et sidebar_footer.php

--- header.php
+++ header.php
@@ -1,10 +1,15 @@
 <!-- <div id="page"> -->
+  
+<head>
   <div id="header">
     <!-- informations du blog -->     
     <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
     <?php bloginfo('description'); ?>
+	</div>
+        
+  <?php get_sidebar(); ?> <!-- ouvrir sidebar.php -->
+
 
         
-    </header> 
-	</div>
+</header> 
 	
